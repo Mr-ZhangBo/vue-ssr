cb6f92f1a836f4f5fc9e5f5c852e650193f9e205
zhangbo <15947658171@163.com>
2020-04-07 22:12:12 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 // import router from './router'
 import { createRouter } from './router'
+import { createStore } from './store'
 
 Vue.config.productionTip = false
 
 export function createApp (context) {
   const router = createRouter()
+  const store = createStore()
   const app = new Vue({
     router,
+    store,
     context,
     render: h => h(App)
   }).$mount('#app')
 
-  return { app, router }
+  return { app, router, store }
 }
